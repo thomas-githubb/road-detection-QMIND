a5a5ed1aa7366055a9329419efbf7e26469d2835
Thomas <thomas.dermengea@gmail.com>
2025-01-08 17:24:06 -0500

    starting for website

---
+++ my-next-app/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+// pages/about.tsx
+export default function About() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background text-foreground">
+      <h1 className="text-3xl font-bold">About the Project</h1>
+    </div>
+  );
+}
